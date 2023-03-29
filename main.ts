516e6c2beb0d9bc9b2f8839f55dcbc2b184af65b
cheeky sim <48182295+cheekysim@users.noreply.github.com>
2023-03-29 09:02:06 +0100

    changes

--- main.ts
+++ main.ts
@@ -10,6 +10,8 @@ import {
 	WorkspaceLeaf,
 } from "obsidian";
 import * as https from "https";
+import * as fs from "fs";
+import { Configuration, OpenAIApi, CreateImageRequestSizeEnum } from "openai";
 // Remember to rename these classes and interfaces!
 
 interface Settings {
@@ -32,15 +34,31 @@ export default class ChatMD extends Plugin {
 	settings: Settings;
 
 	async onload() {
-		console.log("loading chatmd")
+		console.log("loading chatmd");
 		await this.loadSettings();
 
-		this.registerView('chatmd', (leaf) => {
+		this.registerView("chatmd", (leaf) => {
 			return new ChatMDView(leaf);
 		});
 
 		this.addRibbonIcon("dice", "Chat MD", () => {
 			loadSidebar.call(this);
+		});
+
+		this.addRibbonIcon("dice", "Generate Image", () => {
+			console.log("Clicked");
+			const config = {
+				apikey: this.settings.apikey,
+				size: 256
+			};
+			getImage("Dog", config, async (data) => {
+				// Get image from data (url)
+				const image = await fetch(data);
+				const blob = await image.blob();
+				console.log(blob.type);
+				// Create new file
+
+			});
 		});
 
 		this.addCommand({
@@ -101,8 +119,8 @@ export default class ChatMD extends Plugin {
 			name: "Open Chat MD Sidebar",
 			callback: () => {
 				loadSidebar.call(this);
-			}
-		})
+			},
+		});
 
 		this.addSettingTab(new SettingsTab(this.app, this));
 	}
@@ -123,7 +141,7 @@ export default class ChatMD extends Plugin {
 }
 
 async function loadSidebar(this: ChatMD) {
-	const leaves = this.app.workspace.getLeavesOfType('chatmd');
+	const leaves = this.app.workspace.getLeavesOfType("chatmd");
 	if (leaves.length > 0) {
 		this.app.workspace.revealLeaf(leaves[0]);
 	} else {
@@ -131,7 +149,7 @@ async function loadSidebar(this: ChatMD) {
 		let leaf = this.app.workspace.getRightLeaf(false);
 		if (leaf) {
 			leaf.setViewState({
-				type: 'chatmd',
+				type: "chatmd",
 				active: true,
 			});
 		}
@@ -184,26 +202,81 @@ async function getText(
 	req.end();
 }
 
+async function getImage(
+	prompt: string,
+	config: { apikey: string; size: number },
+	callback: { (data: string): void; (arg0: string): void }
+) {
+	console.log("Getting Image");
+	const configuration = new Configuration({
+		apiKey: config.apikey,
+	});
+	let size: CreateImageRequestSizeEnum = CreateImageRequestSizeEnum._256x256;
+	if (config.size >= 1024) size = CreateImageRequestSizeEnum._1024x1024;
+	else if (config.size >= 512) size = CreateImageRequestSizeEnum._512x512;
+	const openai = new OpenAIApi(configuration);
+	const response = await openai.createImage({
+		prompt: prompt,
+		n: 1,
+		response_format: "url",
+		size: size,
+	});
+	if (response.data.data[0].url) {
+		callback(response.data.data[0].url as string);
+	}
+	// const data = JSON.stringify({
+	// 	prompt: prompt,
+	// 	n: 1,
+	// 	size: `${config.size}x${config.size}`,
+	// });
+
+	// const xhr = new XMLHttpRequest();
+	// xhr.withCredentials = true;
+
+	// xhr.addEventListener("readystatechange", function () {
+	// 	if (this.readyState === this.DONE) {
+	// 		callback(this.responseText);
+	// 	}
+	// });
+
+	// xhr.open("POST", "https://api.openai.com/v1/images/generations");
+	// xhr.setRequestHeader("Content-Type", "application/json");
+	// xhr.setRequestHeader("Access-Control-Allow-Origin", "*")
+	// xhr.setRequestHeader("Authorization", `Bearer ${config.apikey}`);
+
+	// xhr.send(data);
+}
+
 class ChatMDView extends ItemView {
 	constructor(leaf: WorkspaceLeaf) {
 		super(leaf);
 		this.icon = "dice";
 		this.containerEl.empty();
-		this.containerEl.createEl("h1", { text: this.getDisplayText() }).addClass("chatmd-header");
-		const container = this.containerEl.createDiv("chatmd-message-container");
+		this.containerEl
+			.createEl("h1", { text: this.getDisplayText() })
+			.addClass("chatmd-header");
+		const container = this.containerEl.createDiv(
+			"chatmd-message-container"
+		);
 		for (let index = 0; index < 10; index++) {
-			const msg = container.createEl("p", { text: "Welcome to Chat MD!" });
+			const msg = container.createEl("p", {
+				text: "Welcome to Chat MD!",
+			});
 			msg.setAttribute("data-role", "ai");
 			msg.addClass("chatmd-message");
 			const msg2 = container.createEl("p", { text: "This is the USER!" });
 			msg2.setAttribute("data-role", "user");
 			msg2.addClass("chatmd-message");
 		}
-		const inputForm = this.containerEl.createEl('form')
-		inputForm.addClass('chatmd-input-form')
-		inputForm.createEl('button', { text: "New Chat" }).addClass('chatmd-new-chat')
-		inputForm.createEl("input", { text: "Input Test" }).addClass('chatmd-input')
-		inputForm.createEl('button', { text: "Send" }).addClass('chatmd-send')	
+		const inputForm = this.containerEl.createEl("form");
+		inputForm.addClass("chatmd-input-form");
+		inputForm
+			.createEl("button", { text: "New Chat" })
+			.addClass("chatmd-new-chat");
+		inputForm
+			.createEl("input", { text: "Input Test" })
+			.addClass("chatmd-input");
+		inputForm.createEl("button", { text: "Send" }).addClass("chatmd-send");
 	}
 
 	getViewType(): string {
@@ -253,16 +326,22 @@ class SettingsTab extends PluginSettingTab {
 					.onChange(async (value) => {
 						// Check if input is valid
 						if (parseInt(value) > 4096) {
-							new Notice("Max Tokens cannot be greater than 4096");
+							new Notice(
+								"Max Tokens cannot be greater than 4096"
+							);
 							return;
 						} else if (parseInt(value) == 0) {
 							new Notice("Max Tokens cannot be 0");
 							return;
 						}
 						// Update Slider
-						const settingItem = containerEl.findAll('.setting-item')[1]
-						const input = settingItem.querySelector<HTMLInputElement>('input[type=range]')
-						if (input) input.value = value.toString()
+						const settingItem =
+							containerEl.findAll(".setting-item")[1];
+						const input =
+							settingItem.querySelector<HTMLInputElement>(
+								"input[type=range]"
+							);
+						if (input) input.value = value.toString();
 						// Update setting value
 						this.plugin.settings.max_tokens = parseInt(value);
 						await this.plugin.saveSettings();
@@ -275,9 +354,13 @@ class SettingsTab extends PluginSettingTab {
 					.setDynamicTooltip()
 					.onChange(async (value) => {
 						// Update Text Input
-						const settingItem = containerEl.findAll('.setting-item')[1]
-						const input = settingItem.querySelector<HTMLInputElement>('input[type=text]')
-						if (input) input.value = value.toString()
+						const settingItem =
+							containerEl.findAll(".setting-item")[1];
+						const input =
+							settingItem.querySelector<HTMLInputElement>(
+								"input[type=text]"
+							);
+						if (input) input.value = value.toString();
 						// Update setting value
 						this.plugin.settings.max_tokens = value;
 						await this.plugin.saveSettings();
